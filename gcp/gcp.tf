b0a3558bc95ebb24323c73762fcedaa16c1d8b0c
Danny Sievers <danjsievers@gmail.com>
2019-12-10 10:00:42 -0600

    fix formatting

--- gcp/gcp.tf
+++ gcp/gcp.tf
@@ -2,11 +2,11 @@
 module "project" {
   source = "git::https://github.com/dannysievers/gcp-project.git//project"
 
-  project_name = "go-api-gke"
+  project_name       = "go-api-gke"
   billing_account_id = var.gcp_billing_account_id
   org_id             = var.gcp_org_id
   region             = "us-central1"
-  gcp_services       = [
+  gcp_services = [
     "cloudresourcemanager.googleapis.com",
     "cloudbilling.googleapis.com",
     "compute.googleapis.com",
