9988635db879eddd3c9f42814da9d0828532dfdb
Danny Sievers <danjsievers@gmail.com>
2019-12-09 23:56:33 -0600

    fix formatting

--- gcp/gcp.tf
+++ gcp/gcp.tf
@@ -2,7 +2,7 @@
 module "project" {
   source = "git::https://github.com/dannysievers/gcp-project.git//project"
 
-  project_name       = "go-api-gke"
+  project_name = "go-api-gke"
 #   billing_account_id = var.gcp_billing_account_id
 #   org_id             = var.gcp_org_id
 #   region             = "us-central1"
